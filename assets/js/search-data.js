f9e4bcc88fdf03333aa78dd10ca2c02c68e6023c
Smart-Ag <44210435+Smart-Ag@users.noreply.github.com>
2026-08-31 00:48:14 +0000

    Deploying to gh-pages from @ Smart-Ag/Smart-Ag.github.io@b0243edbca1f4e0a60de8069aba648ebeb5bb3af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -242,6 +242,21 @@ ninja.data = [{
           description: "实验室全体成员参与",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Presentions_2026.07.27/";
+            },},{id: "projects-实验室组会",
+          title: '实验室组会',
+          description: "实验室全体成员参与",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Presentions_2026.08.03/";
+            },},{id: "projects-实验室组会",
+          title: '实验室组会',
+          description: "实验室全体成员参与",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Presentions_2026.08.12/";
+            },},{id: "projects-实验室组会",
+          title: '实验室组会',
+          description: "实验室全体成员参与",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Presentions_2026.08.28/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
